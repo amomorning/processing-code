1b4c336a909da94f4e6e9360c08af45f976ffe2c
Yichen Mo <amomorning@gmail.com>
2020-07-06 15:28:04 +0800

    Update README.md

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 Simple processing examples separated by different branches.
 
-### master branch 
+### master 
 As a template, it's also an example from [processing.org](https://processing.org/examples/mousefunctions.html)
 ![](fig/HWXu8aTBde.gif)
 
-### Reaction-Diffusion
+### diffusion
+Reaction-Diffusion
 ![](https://github.com/amomorning/processing-code/blob/diffusion/fig/SKo2w2KJSda.png)
--- src/main/java/main/Show.java
+++ src/main/java/main/Show.java
@@ -10,12 +10,6 @@ import processing.core.PApplet;
  */
 public class Show extends PApplet {
 
-    float bx, by;
-    int boxSize = 75;
-    boolean overBox = false;
-    boolean locked = false;
-    float xOffset = 0.0f;
-    float yOffset = 0.0f;
 
     public void settings() {
         size(1000, 1000, P2D);
@@ -23,59 +17,19 @@ public class Show extends PApplet {
     }
 
     public void setup() {
-        bx = width/2.0f;
-        by = height/2.0f;
-        rectMode(RADIUS);
+        background(255, 122, 0);
+        mouseX = -0x3f;
+        mouseY = -0x3f;
     }
-
 
 
     public void draw(){
-        background(0);
-
-        // Test if the cursor is over the box
-        if (mouseX > bx-boxSize && mouseX < bx+boxSize &&
-                mouseY > by-boxSize && mouseY < by+boxSize) {
-            overBox = true;
-            if(!locked) {
-                stroke(255);
-                fill(153);
-            }
-        } else {
-            stroke(153);
-            fill(153);
-            overBox = false;
-        }
-
         // Draw the box
-        rect(bx, by, boxSize, boxSize);
-    }
-
-
-    public void keyPressed() {
-
-    }
-
-    public void mousePressed() {
-        if(overBox) {
-            locked = true;
-            fill(255, 255, 255);
-        } else {
-            locked = false;
-        }
-        xOffset = mouseX-bx;
-        yOffset = mouseY-by;
-
-    }
-
-    public void mouseDragged() {
-        if(locked) {
-            bx = mouseX-xOffset;
-            by = mouseY-yOffset;
+        if(mouseX != -0x3f && mouseY != -0x3f) {
+            rect(mouseX, mouseY, 200, 100);
         }
     }
 
-    public void mouseReleased() {
-        locked = false;
-    }
+
+
 }
