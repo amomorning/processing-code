ac73699d2ae0c59820f6c6bb5305badd72de1221
Yichen Mo <amomorning@gmail.com>
2020-08-12 20:00:02 +0800

    Add processing drawings

--- src/main/node/js/gui.js
+++ src/main/node/js/gui.js
@@ -1,19 +1,49 @@
 "use strict";
 var controls = new function () {
-    this.LinNumX = 3;
-    this.LinNumY = 4;
-    this.add = function() {
-        var object = randomCube();
-        scene.add(object);
-        objects.push(object);
+    this.sizPoi = 0.5;
+    this.shaPoi = 'square';
+    this.angPoi = 0.0;
+    this.typeColor = '#000000';
+
+    this.LinNum_X = 3;
+    this.LinNum_Y = 4;
+    this.shiL_X = false;
+    this.shiL_Y = false;
+    this.shiP_X = false;
+    this.shiP_Y = false;
+    this.selPoi = this.LinNum_X * this.LinNum_Y;
+    
+
+    this.update = function() {
+        renderer.setSize(300, 300);
+        socket.emit('parametersExchange', JSON.stringify(this));
     };
-    this.hello = function() {
-        socket.emit('event', 'hello from gui');
-    };
+    this.save = function() {
+        saveAsImage();
+    }
 }
 
 var gui = new dat.GUI();
-gui.add(controls, 'LinNumX', 2, 13).step(1);
-gui.add(controls, 'LinNumY', 2, 13).step(1);
-gui.add(controls, 'add');
-gui.add(controls, 'hello');
+var element = gui.addFolder('Element Parameters');
+element.add(controls, 'sizPoi', 0, 1);
+element.add(controls, 'shaPoi', [ 'square', 'circle', 'triangle' ] );
+element.add(controls, 'angPoi', 0, 360).step(1);
+element.addColor(controls, 'typeColor');
+element.open();
+
+var space = gui.addFolder('Space Parameters');
+space.add(controls, 'LinNum_X', 2, 13).step(1);
+space.add(controls, 'LinNum_Y', 2, 13).step(1);
+space.add(controls, 'shiL_X');
+space.add(controls, 'shiL_Y');
+space.add(controls, 'shiP_X');
+space.add(controls, 'shiP_Y');
+space.add(controls, 'selPoi', 0, controls.LinNum_X * controls.LinNum_Y).step(1);
+
+space.open();
+
+
+var util = gui.addFolder('Utils');
+util.add(controls, 'update');
+util.add(controls, 'save');
+util.open();
